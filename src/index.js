f7ac85cc48dc7f10850a85d3c2c74fa5ece26e25
AJ Hsu <ajhsu@kkbox.com>
2017-04-10 17:06:00 +0800

    Update default pre-recorded script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import CastDeviceEmulator from './castDeviceEmulator';
 const emulator = new CastDeviceEmulator();
-emulator.loadScript(require('../recorded-scripts/example.json'));
+emulator.loadScript(require('../recorded-scripts/unit-testing.json'));
 emulator.start();
