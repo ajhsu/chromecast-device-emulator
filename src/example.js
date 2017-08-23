92412bd740c98f4b897cc61a93cbac0e8c1bc1de
AJ Hsu <ajhsu@kkbox.com>
2017-08-23 17:42:22 +0800

    Create the entry point file of standalone usage

--- src/example.js
+++
@@ -1,5 +0,0 @@
-import CastDeviceEmulator from './index';
-const emulator = new CastDeviceEmulator();
-emulator.loadScenario(require('../scenarios/unit-testing.json'));
-emulator.start();
-emulator.close();
