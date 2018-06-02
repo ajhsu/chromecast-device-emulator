97de2b25916dd843d087d6826791bf92369a4b55
AJ Hsu <ajhsu@kkbox.com>
2018-06-02 15:13:41 +0800

    refactor: Update example usage

--- examples/standalone/example.js
+++ examples/standalone/example.js
@@ -1,4 +1,4 @@
-import CastDeviceEmulator from './';
+const CastDeviceEmulator = require('chromecast-device-emulator');
 const emulator = new CastDeviceEmulator();
 
 emulator.loadScenario(require('../scenarios/example.json'));
